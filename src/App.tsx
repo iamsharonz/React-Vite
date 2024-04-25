6145b15146fb3d6d7a261db00f03b805f5b72637
sharon <sharon.joseph@thinkpalm.com>
2024-04-25 11:16:01 +0530

    Lecture 3: Passing function via Props

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,13 @@ import ListGroup from './components/ListGroup'
 
 function App()
 {
+  const handSelectItems = (item: string) =>
+  {
+    console.log(item);
+  }
+
   let items = ['Africa', 'Antartica', 'Anasthesia'];
-  return <div><ListGroup items = {items} heading = "Cities" /></div>
+  return <div><ListGroup items = {items} heading = "Cities" onSelectItem={handSelectItems}/></div>
 }
 
 export default App;
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -3,22 +3,23 @@ import { MouseEvent, useState } from "react";
 interface Props{
     items: string[];
     heading: string;
+    onSelectItem: (item: string) => void;
 }
 
-function ListGroup({items, heading}: Props)
+function ListGroup({items, heading, onSelectItem}: Props)
 {
     //const handleClick = (event: MouseEvent) => console.log(event);
     const [selectedIndex, setSelectedIndex] = useState(-1);
 
     return(
-         //  fragements - as react cant return multiple elements 
+         //  fragments - as react cant return multiple elements 
         <>
     <h1>{heading}</h1>
     {/* {items.length === 0 ? <p>No items found!</p> : null} */}
     {items.length === 0 && <p>No items found</p>}
     <ul className="list-group">
         {items.map((item, index) => (
-            <li className={selectedIndex === index ? "list-group-item active" : "list-group-item"} key={item} onClick={() => setSelectedIndex(index)}>{item}</li>
+            <li className={selectedIndex === index ? "list-group-item active" : "list-group-item"} key={item} onClick={() => { setSelectedIndex(index); onSelectItem(item);} }>{item}</li>
         ))}
   </ul>
   </>
